0626e2fb5d48046b2a39301710aaf061c74f9318
evgeniy <eugenio7002@gmail.com>
2026-06-09 15:11:08 +0300

    add the /api/auth/validate endpoint to the .permitAll() configuration list

--- src/main/java/com/innowise/authservice/config/SecurityConfig.java
+++ src/main/java/com/innowise/authservice/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/token", "/api/auth/refresh").permitAll()
+                        .requestMatchers("/api/auth/token", "/api/auth/refresh", "/api/auth/validate").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(ex -> ex
